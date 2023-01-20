99dd6048faed77444c2602d812d6465f56d6ac29
Sionylen <sionylenpierce@outlook.com>
2023-01-19 23:16:25 -0600

    first commit

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using Testing.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace Testing
 {
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
     }
 }
--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public int ProductsID { get; set; }
+        public int ProductID { get; set; }
         public string Name { get; set;}
         public double Price { get; set;}
 
